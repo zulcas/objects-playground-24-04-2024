13a797be80c36908b693bf31cf0077312a7d8adc
omiras <oscar.miras@gmail.com>
2023-10-16 23:28:14 +0200

    ex2

--- ex2.js
+++ ex2.js
@@ -9,6 +9,8 @@ let persona = {
 
 /**
  * Escribe una instrucción para cambiar la propiedad 'apellidos' de la persona. Queremos cambiar sus apellidos a 'Rudolph'
+ *
+ * Resultado esperado: https://oscarm.tinytake.com/msc/ODgxMTQ2MV8yMjI0MDgxMQ
  */
 
 // Aquí a continuación tu instrucción JavaScript
