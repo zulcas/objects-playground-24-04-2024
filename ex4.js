867e54a35898082926e70e57a981ee0a2b47b235
omiras <oscar.miras@gmail.com>
2023-10-16 23:38:46 +0200

    format

--- ex4.js
+++ ex4.js
@@ -6,13 +6,15 @@
  * Implementa el método "aprender"; que está parcialmente implementado. LA función recibe por parámetro un string, con el nuevo conocimiento aprendido. Debe actualizar la propiedad 'conocimientos' con dicho nuevo string.
  */
 let estudiante = {
-    nombre: "Edu",
-    conocimientos: [],
-    aprender: function (nuevoConocimiento) {
-        // una línia de código
-    }
+  nombre: "Edu",
+  conocimientos: [],
+  aprender: function (nuevoConocimiento) {
+    // una línia de código
+  },
+};
 
-}
-
-estudiante.aprender('HTML')
-console.log("Debería aparecer aquí que el estudiante ha aprendido HTML:", estudiante.conocimientos)
+estudiante.aprender("HTML");
+console.log(
+  "Debería aparecer aquí que el estudiante ha aprendido HTML:",
+  estudiante.conocimientos
+);
--- ex5.js
+++ ex5.js
@@ -3,50 +3,46 @@
  */
 
 let pez1 = {
-    nombre: "Dory",
-    color: 'azul'
-
-}
+  nombre: "Dory",
+  color: "azul",
+};
 
 let pez2 = {
-    nombre: "Nemo",
-    color: 'naranja'
-}
+  nombre: "Nemo",
+  color: "naranja",
+};
 
 let pez3 = {
-    nombre: "Sipho",
-    color: 'negro'
-}
+  nombre: "Sipho",
+  color: "negro",
+};
 
 /**
  * Disponemos de un array de 3 objetos. Concretamente, son 3 peces.
  * Queremos implementar una función que nos diga si cierto pez está en el array.
- * 
+ *
  * 1. ¿Cómo debemos actualizar la variable pezEncontrado a cada iteración del bucle?
  * 2. ¿Cómo debemos actualizar la variale 'indicePez' a cada iteración del bucle?
- * 
+ *
  * Ayudate de chivatos y del depurador de visual Studio para ver que sucede dentro del bucle a cada iteración
  */
 
-let banco_peces = [pez1, pez2, pez3]
+let banco_peces = [pez1, pez2, pez3];
 
 function buscarPez(banco_peces, pezBuscado) {
-    let pezEncontrado = false
-    let indicePez = 0
+  let pezEncontrado = false;
+  let indicePez = 0;
 
-    while(!pezEncontrado && indicePez<=banco_peces.length){
-        
-    }
+  while (!pezEncontrado && indicePez <= banco_peces.length) {}
 
-    return pezEncontrado;
+  return pezEncontrado;
 }
 
 // Esta llamada debería devolver 'true'
-// console.log(buscarPez(banco_peces, 'Nemo')); 
+// console.log(buscarPez(banco_peces, 'Nemo'));
 
 // Esta llamada debería devolver 'true'
 // console.log(buscarPez(banco_peces, 'Sipho'));
 
 // Esta llamada debería devolver 'false'
 // console.log(buscarPez(banco_peces, 'Tenacitas'));
-
