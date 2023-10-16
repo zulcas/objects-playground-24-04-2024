1a16018470f5d412e83fcfb4856aa3245b6606f8
omiras <oscar.miras@gmail.com>
2023-10-16 23:41:37 +0200

    ex4

--- ex4.js
+++ ex4.js
@@ -4,6 +4,8 @@
 
 /**
  * Implementa el método "aprender"; que está parcialmente implementado. LA función recibe por parámetro un string, con el nuevo conocimiento aprendido. Debe actualizar la propiedad 'conocimientos' con dicho nuevo string.
+ *
+ * Resultado esperado: https://oscarm.tinytake.com/msc/ODgxMTQ3OV8yMjI0MDgyOQ
  */
 let estudiante = {
   nombre: "Edu",
